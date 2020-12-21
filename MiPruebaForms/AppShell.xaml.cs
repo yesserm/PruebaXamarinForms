ad2a9cd904e8b6edc4280efcc5912b789f2fe62f
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-12-21 16:12:59 -0600

    Agrega el proyecto Xamarin Forms y el sln

---
+++ MiPruebaForms/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MiPruebaForms.ViewModels;
+using MiPruebaForms.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MiPruebaForms
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ MiPruebaForms/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MiPruebaForms.Models;
+using MiPruebaForms.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MiPruebaForms.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
