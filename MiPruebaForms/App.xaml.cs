52cc03a2e99b0c8ae772a99cf67429104756d92f
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-12-22 06:12:32 -0600

    App center para build

--- MiPruebaForms/App.xaml.cs
+++ MiPruebaForms/App.xaml.cs
@@ -3,6 +3,9 @@ using MiPruebaForms.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace MiPruebaForms
 {
@@ -19,6 +22,10 @@ namespace MiPruebaForms
 
         protected override void OnStart()
         {
+            AppCenter.Start("ios=72f150a9-8593-4a58-a4e3-9dc9db3dffaa;" +
+                  "uwp=72f150a9-8593-4a58-a4e3-9dc9db3dffab;" +
+                  "android=72f150a9-8593-4a58-a4e3-9dc9db3dffac;",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
